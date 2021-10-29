ff72f68a65796e8dea092df52538d2c2455728c6
Raddiactive <68911701+Raddiactive@users.noreply.github.com>
2021-10-29 18:34:38 +0200

    Add files via upload

---
+++ requeriments.py
@@ -0,0 +1,14 @@
+import os
+from time import sleep
+
+os.system("clear")
+
+print("\033[1;37;46m Empezando instalación de requests ❯\033[0;0m\n")
+os.system("pip3 install -U requests")
+
+print("\n\033[1;37;46m Empezando instalación de bs4 ❯\033[0;0m\n")
+os.system("pip3 install -U beautifulsoup4")
+
+print("\n\033[1;32;40mFinalizado.\033[0;0m")
+sleep(2)
+os.system("clear")
